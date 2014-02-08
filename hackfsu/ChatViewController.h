a7ae333eede34eb13060d0ebbd87421b896a50ab
Michelle <mrojass1028@gmail.com>
2014-02-07 20:40:09 -0500

    Master updated.

---
+++ hackfsu/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  hackfsu
+//
+//  Created by Michelle Rojas on 2/7/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UITableViewController
+
+@end
---
+++ hackfsu/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  hackfsu
+//
+//  Created by Michelle Rojas on 2/7/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewController : UITableViewController
+
+@end
