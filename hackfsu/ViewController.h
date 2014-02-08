e70227047c021e2e4a12744c7a09b8948a0be693
Logan Isitt <laisitt@gmail.com>
2014-02-07 19:55:43 -0500

    Initial Commit.

---
+++ hackfsu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hackfsu
+//
+//  Created by Logan Isitt on 2/7/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ hackfsu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hackfsu
+//
+//  Created by Logan Isitt on 2/7/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
