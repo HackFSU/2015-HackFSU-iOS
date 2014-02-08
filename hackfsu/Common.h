0644de142f445ed440b8bda0cd939227086a6392
Logan Isitt <laisitt@gmail.com>
2014-02-07 21:47:05 -0500

    Project Fixed

--- hackfsu/Common.h
+++ hackfsu/Common.h
@@ -1,14 +1,1 @@
-//
-//  Common.h
-//  hackfsu
-//
-//  Created by Logan Isitt on 2/7/14.
-//  Copyright (c) 2014 Isitt Inc. All rights reserved.
-//
 
-#ifndef __hackfsu__Common__
-#define __hackfsu__Common__
-
-#include <iostream>
-
-#endif /* defined(__hackfsu__Common__) */
