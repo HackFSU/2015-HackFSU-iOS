c628345a6131bed15bc2095b97e68a2cfb84b007
Logan Isitt <laisitt@gmail.com>
2014-03-24 12:27:40 -0400

    Modified tabs.

---
+++ hackfsu/EventLocation.h
@@ -0,0 +1,13 @@
+//
+//  EventLocation.h
+//  hackfsu
+//
+//  Created by Logan Isitt on 3/23/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventLocation : NSObject
+
+@end
---
+++ hackfsu/EventLocation.m
@@ -0,0 +1,13 @@
+//
+//  EventLocation.m
+//  hackfsu
+//
+//  Created by Logan Isitt on 3/23/14.
+//  Copyright (c) 2014 Isitt Inc. All rights reserved.
+//
+
+#import "EventLocation.h"
+
+@implementation EventLocation
+
+@end
