0650a9bcc41d654050fed85a499809208731d9bf
Logan Isitt <laisitt@gmail.com>
2015-01-28 21:46:36 -0500

    New project files

---
+++ HackFSU/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HackFSU
+//
+//  Created by Logan Isitt on 1/28/15.
+//  Copyright (c) 2015 Logan Isitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
